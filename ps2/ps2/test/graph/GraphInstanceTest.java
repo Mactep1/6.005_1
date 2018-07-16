e2a17446a9fb78beac81aa704fca8ee06a787c99
mactep <421790048@qq.com>
2018-07-16 15:59:11 +0800

    revised GraphInstanceTest.java

--- ps2/ps2/test/graph/GraphInstanceTest.java
+++ ps2/ps2/test/graph/GraphInstanceTest.java
@@ -6,6 +6,10 @@ package graph;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -44,8 +48,98 @@ public abstract class GraphInstanceTest {
     @Test
     public void testAdd() {
     	String vertice="v";
+    	String vertice1="v";
     	assertTrue(emptyInstance().add(vertice));
+    	assertFalse(emptyInstance().add(vertice));
+    	assertTrue(emptyInstance().add(vertice1));
     }
     // TODO other tests for instance methods of Graph
     
+    public void testSet() {
+    	String s1="v1";
+    	String s2="v2";
+    	int w1=0;
+    	int w2=1;
+    	int w3=-1;
+    	
+    	int r1=emptyInstance().set(s1, s1, w1);
+    	int r2=emptyInstance().set(s1, s1, w2);
+    	int r3=emptyInstance().set(s1, s1, w3);
+    	int r4=emptyInstance().set(s1, s2, w1);
+    	int r5=emptyInstance().set(s1, s2, w2);
+    	int r6=emptyInstance().set(s1, s2, w3);
+    	int r7=emptyInstance().set(s1, s2, w1);
+    	
+    	assertEquals(r1, 0);
+    	assertEquals(r2, 0);
+    	assertEquals(r3, 1);
+    	assertEquals(r4, -1);
+    	assertEquals(r5, 0);
+    	assertEquals(r6, 1);
+    	assertEquals(r7, -1);
+    }
+    
+    public void testRemove() {
+    	String vertice="v";
+    	
+    	assertFalse(emptyInstance().remove(vertice));
+    	emptyInstance().add(vertice);
+    	assertTrue(emptyInstance().remove(vertice));
+    }
+    
+    public void testVertices() {
+    	String s1="v1";
+    	String s2="v2";
+    	Set<String> set1=new HashSet<String>();
+    	set1.add(s1);
+    	assertEquals(Collections.EMPTY_SET, emptyInstance().vertices());
+    	emptyInstance().add(s1);    	
+    	assertEquals(set1, emptyInstance());
+    }
+    
+    public void testSources() {
+    	String s1="v1";
+    	String s2="v2";
+    	String s3="v3";
+    	int w1=0;
+    	int w2=1;
+    	int w3=-1;
+    	
+    	emptyInstance().set(s1, s1, w2);
+    	emptyInstance().set(s1, s2, w2);
+    	emptyInstance().set(s3, s2, w3);
+    	
+    	Map<String, Integer> m0=new HashMap<>();
+    	m0.put(s1, w2);
+    	Map<String, Integer> m1=new HashMap<>();
+    	m1.put(s1, w2);
+    	m1.put(s3, w3);
+    	
+    	assertEquals(m0, emptyInstance().sources(s1));
+    	assertEquals(m1, emptyInstance().sources(s2));
+    	assertEquals(Collections.EMPTY_MAP, emptyInstance().sources(s3));
+    }
+    
+    public void assertTargets() {
+    	String s1="v1";
+    	String s2="v2";
+    	String s3="v3";
+    	int w1=0;
+    	int w2=1;
+    	int w3=-1;
+    	
+    	emptyInstance().set(s1, s1, w2);
+    	emptyInstance().set(s2, s1, w2);
+    	emptyInstance().set(s2, s3, w3);
+    	
+    	Map<String, Integer> m0=new HashMap<>();
+    	m0.put(s1, w2);
+    	Map<String, Integer> m1=new HashMap<>();
+    	m1.put(s1, w2);
+    	m1.put(s3, w3);
+    	
+    	assertEquals(m0, emptyInstance().targets(s1));
+    	assertEquals(m1, emptyInstance().targets(s2));
+    	assertEquals(Collections.EMPTY_MAP, emptyInstance().targets(s3));
+    }
 }
